317670889f05cf2583ffc75369efe56b16e95291
MExD69 <126236193+MExD69@users.noreply.github.com>
2024-04-01 20:00:52 +0000

    12345

--- Projekts/app.py
+++ Projekts/app.py
@@ -7,29 +7,12 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    if request.method == 'POST':
-        nosaukums = request.form.get('nosaukums')
-    else:
-        MinWage = None
-    if request.method == 'POST':
-        MinWage = request.form.get('MinWage')
-    else:
-        nosaukums = None
-    if request.method == 'POST':
-        MaxWage = request.form.get('MaxVage')
-    else:
-        MaxWage = None
-    if request.method == 'POST':
-        Location = request.form.get('Location')
-    else:
-        Location = None
-    
-    
-    
-    table = helpers.get_data_gov_lv(nosaukums,MinWage,MaxWage,Location)
-
+    nosaukums = request.form.get('nosaukums') if request.method == 'POST' else None
+    MinWage = request.form.get('MinWage') if request.method == 'POST' else None
+    MaxWage = request.form.get('MaxWage') if request.method == 'POST' else None  # fixed typo here
+    Location = request.form.get('Location') if request.method == 'POST' else None
+    table = helpers.get_data_gov_lv(nosaukums, MinWage, MaxWage, Location)
     return render_template("data.html", table=table)
-
 
 @app.route("/register")
 
--- Projekts/helpers.py
+++ Projekts/helpers.py
@@ -4,27 +4,23 @@ import json
 from flask import request
 
 def get_data_gov_lv(nosaukums=None,MinWage=None,MaxWage=None,Location=None):
-    #filtrs[]=insert(nosaukums)
-    n=0
     start_url = 'https://data.gov.lv/dati/lv/api/3/action/datastore_search_sql?sql=SELECT * FROM "7f68f6fc-a0f9-4c31-b43c-770e97a06fda"'
     end_url = ' ORDER BY _id'
     query = start_url
-    if nosaukums is not None or MinWage is not None or MaxWage is not None or Location is not None or nosaukums != '' or MinWage != '' or MaxWage != '' or Location != '' and n != 0:
-        query += ' WHERE'
-    print(nosaukums)
-    if nosaukums is not None and nosaukums != "":
-        query = query + ' "Vakances nosaukums" LIKE \'' + nosaukums + '%\''
-    if MinWage is not None and MinWage != "":
-        query = query  + ' AND "Alga no" >='+ MinWage
-    
-    if MaxWage is not None and MaxWage != "":
-        query = query+ ' AND "Alga līdz" <=' + MaxWage
-    if Location is not None and Location != "":
-        query = query+ ' AND "Vieta" LIKE \'' + Location + '%\''
+    conditions = []
+    if nosaukums:
+        conditions.append(f' "Vakances nosaukums" LIKE \'{nosaukums}%\'')
+    if MinWage:
+        conditions.append(f' "Alga no" >= {MinWage}')
+    if MaxWage:
+        conditions.append(f' "Alga līdz" <= {MaxWage}')
+    if Location:
+        conditions.append(f' "Vieta" LIKE \'%{Location}%\'')
+    if conditions:
+        query += ' WHERE' + ' AND'.join(conditions)
     url = query + end_url
     print(url)
     response = requests.get(url)
-    #print(response)
     data = response.json()
 
 
@@ -43,5 +39,4 @@ def get_data_gov_lv(nosaukums=None,MinWage=None,MaxWage=None,Location=None):
         table += "<td><a href="+row["Vakances paplašināts apraksts"]+">More info</td>"
         table += "</tr>"
     table += "</table>"
-    n +=1
     return table
